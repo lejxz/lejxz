779cfdd30f78fa3634f40705f740b4ef11592b4f
寻缘者 <delantarlejuene@gmail.com>
2026-03-08 21:06:33 +0800

    Update

--- assets/js/data-loader.js
+++
@@ -1,8 +0,0 @@
-export async function getPortfolioData() {
-  const res = await fetch("./assets/data/portfolio.json");
-  if (!res.ok) {
-    throw new Error("Failed to load portfolio data.");
-  }
-
-  return res.json();
-}
